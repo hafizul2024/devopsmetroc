7ebeebd775b9fdcf5117507fbb003b0cbd862212
Harun Rashid <haru5892@DX2T6R2PWW.local>
2024-04-18 18:47:08 -0400

    Add a new line

--- 2ndfile.py
+++ 2ndfile.py
@@ -9,3 +9,4 @@ miles = kilometers * conv_fac
 print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
 print("My name is Yomi")
 print('This is frank line')
+print('This is a new line')
